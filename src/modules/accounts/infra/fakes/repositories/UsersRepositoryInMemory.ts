79a60f5513ecad6d514cc30d733bdfa518533176
Dyego Soriano <dyegodf2@gmail.com>
2022-02-20 20:32:00 -0300

    refactor(adjustments to repositories and entities): adjustments to repositories and entities

--- src/modules/accounts/infra/fakes/repositories/UsersRepositoryInMemory.ts
+++ src/modules/accounts/infra/fakes/repositories/UsersRepositoryInMemory.ts
@@ -1,25 +1,28 @@
-import { prismaClient } from '@infra/prisma/prismaClient'
 import { ICreateUserDTO } from '@modules/accounts/domains/DTOs/ICreateUserDTO'
 import { IUser } from '@modules/accounts/domains/models/IUser'
 import { IUsersRepository } from '@modules/accounts/domains/repositories/IUsersRepository'
 import { User } from '@modules/accounts/entities/Users'
 
-class UsersRepository implements IUsersRepository {
+class UsersRepositoryInMemory implements IUsersRepository {
+  private users: IUser[] = []
+
   async create ({ password, email, name }: ICreateUserDTO): Promise<IUser> {
     const user = new User()
 
     Object.assign(user, { password, email, name })
 
-    return await prismaClient.users.create({ data: user })
+    this.users.push(user)
+
+    return user
   }
 
   async findByEmail (email: string): Promise<IUser> {
-    return (await prismaClient.users.findFirst({ where: { email } })) as IUser
+    return this.users.find(user => user.email === email) as IUser
   }
 
   async findById (id: string): Promise<IUser> {
-    return (await prismaClient.users.findFirst({ where: { id } })) as IUser
+    return this.users.find(user => user.id === id) as IUser
   }
 }
 
-export { UsersRepository }
+export { UsersRepositoryInMemory }
