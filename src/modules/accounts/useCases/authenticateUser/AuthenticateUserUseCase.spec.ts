90a665c07572bcbdb42fbe04d66df8e66755960b
Dyego Soriano <dyegodf2@gmail.com>
2022-06-21 23:08:13 -0300

    test(authenticate user): refactor AuthenticateUserUseCase.spec

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -9,17 +9,22 @@ import { AppError } from '@shared/errors/AppError'
 import { CreateUserUseCase } from '../createUser/CreateUserUseCase'
 import { AuthenticateUserUseCase } from './AuthenticateUserUseCase'
 
-describe('AuthenticateUserUseCase', () => {
-  const createUserPayload = { password: 'Test12345', name: 'Dyego Soriano', email: 'test@email.com' }
-  const loginUserPayload = { password: 'Test12345', email: 'test@email.com' }
+const createUserPayload = { password: 'Test12345', name: 'Dyego Soriano', email: 'test@email.com' }
+const loginUserPayload = { password: 'Test12345', email: 'test@email.com' }
 
+describe('AuthenticateUserUseCase', () => {
   const makeCreateUser = () => {
     const refreshTokensRepository = new RefreshTokensRepositoryInMemory()
     const userRepository = new UsersRepositoryInMemory()
     const hashProvider = new BcryptHashProvider()
     const dateProvider = new DayjsDateProvider()
 
-    const authenticateUserUseCase = new AuthenticateUserUseCase(refreshTokensRepository, userRepository, hashProvider, dateProvider)
+    const authenticateUserUseCase = new AuthenticateUserUseCase(
+      refreshTokensRepository,
+      userRepository,
+      hashProvider,
+      dateProvider
+    )
     const createUserUseCase = new CreateUserUseCase(userRepository, hashProvider)
 
     return { authenticateUserUseCase, createUserUseCase }
@@ -41,13 +46,16 @@ describe('AuthenticateUserUseCase', () => {
 
     await createUserUseCase.execute(createUserPayload)
 
-    await expect(authenticateUserUseCase.execute({ ...loginUserPayload, email: 'invalid@email.com' }))
-      .rejects.toEqual(new AppError('User or password does not match'))
+    await expect(authenticateUserUseCase.execute({ ...loginUserPayload, email: 'invalid@email.com' })).rejects.toEqual(
+      new AppError('User or password does not match')
+    )
 
-    await expect(authenticateUserUseCase.execute({ ...loginUserPayload, password: 'invalidPass123' }))
-      .rejects.toEqual(new AppError('User or password does not match'))
+    await expect(authenticateUserUseCase.execute({ ...loginUserPayload, password: 'invalidPass123' })).rejects.toEqual(
+      new AppError('User or password does not match')
+    )
 
-    await expect(authenticateUserUseCase.execute({ ...loginUserPayload, password: 'invalidPass' }))
-      .rejects.toEqual(new ValidationError('Password must contain at least one number or letter'))
+    await expect(authenticateUserUseCase.execute({ ...loginUserPayload, password: 'invalidPass' })).rejects.toEqual(
+      new ValidationError('Password must contain at least one number or letter')
+    )
   })
 })
