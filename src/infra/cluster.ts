18c05f345fc9bed508a891ea92d437177cf9addc
Dyego Soriano <4140145+dyegosoriano@users.noreply.github.com>
2023-07-25 11:04:34 -0300

    feat(src/infra/cluster.ts): implementing cluster mode

---
+++ src/infra/cluster.ts
@@ -0,0 +1,31 @@
+import cluster from 'cluster'
+import os from 'os'
+
+const runPrimaryProcess = () => {
+  const processesCount = os.cpus().length
+
+  console.log(`Primary ${process.pid} is running`)
+  console.log(`Forking Server with ${processesCount} processes \n`)
+
+  for (let index = 0; index < processesCount; index++) {
+    cluster.fork()
+  }
+
+  cluster.on('exit', (worker, code, _signal) => {
+    if (code !== 0 && !worker.exitedAfterDisconnect) {
+      console.log(`Worker ${worker.process.pid} died... scheduling another one!`)
+      cluster.fork()
+    }
+  })
+}
+
+const runWorkerProcess = async () => {
+  await import('./server')
+
+  // const { default: server } = (await import('./server')) as any
+  // server()
+}
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+cluster.isPrimary && isProduction ? runPrimaryProcess() : runWorkerProcess()
