d8ecc79926dc6fe20e315962ee7b2d5e358552e8
Dyego Soriano <4140145+dyegosoriano@users.noreply.github.com>
2023-09-14 12:08:15 -0300

    ci(.github/*): adjustments to the CI CD process

--- src/infra/express/routes.ts
+++ src/infra/express/routes.ts
@@ -3,10 +3,12 @@ import { Router } from 'express'
 import { authenticateRoutes } from '@modules/accounts/infra/express/authenticate.routes'
 import { usersRoutes } from '@modules/accounts/infra/express/users.routes'
 
+import { version } from '../../../package.json'
+
 const routes = Router()
 
 routes
-  .get('/', (_req, res) => res.json({ message: 'api is working...' }))
+  .get('/', (_req, res) => res.json({ message: 'api is working...', version }))
   .use('/authenticate', authenticateRoutes)
   .use('/users', usersRoutes)
 
