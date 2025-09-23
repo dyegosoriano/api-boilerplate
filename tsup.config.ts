8762955e139e4b1496b1c3e305ff4c5710bffccb
Dyego Soriano <4140145+dyegosoriano@users.noreply.github.com>
2025-09-22 21:31:37 -0300

    build: build adjustments

---
+++ tsup.config.ts
@@ -0,0 +1,10 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { defineConfig } from 'tsup'
+
+// https://tsup.egoist.dev/#what-can-it-bundle
+// https://github.com/egoist/tsup/issues/728
+export default defineConfig({
+  sourcemap: true,
+  silent: true,
+  clean: true
+})
