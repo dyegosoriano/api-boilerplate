2f8ed41e265639250aad54d398415e1637db4416
Dyego Soriano <4140145+dyegosoriano@users.noreply.github.com>
2025-09-30 16:24:15 -0300

    refactor: removal of express and fastify to create new branches

--- src/infra/fastify/routes.ts
+++
@@ -1,9 +0,0 @@
-import type { FastifyInstance } from 'fastify'
-
-const routes = (fastify: FastifyInstance, opts: unknown, next: Function) => {
-  fastify.get('/', async () => ({ message: 'Hello World!' }))
-
-  next()
-}
-
-export { routes }
--- src/infra/server.ts
+++ src/infra/server.ts
@@ -1,27 +1,9 @@
 import 'dotenv/config'
 
-import { AppServer } from './express/app'
-import { prisma } from './prisma/client'
-
-const port = Number(process.env.PORT) || 3333
-const app = new AppServer()
-
-const server = app.start(port)
-
 function gracefulShutdown(_code: string) {
   return (event: any) => {
     console.info(`${event} signal received with code ${event}`)
     console.info('Closing http server...')
-
-    server.close(async () => {
-      console.info('Http server closed.')
-
-      console.info('Closing database connection...')
-      await prisma.$disconnect()
-      console.info('Database connection closed.')
-
-      process.exit(1)
-    })
   }
 }
 
